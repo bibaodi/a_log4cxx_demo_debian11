2933149e03807b86d78ab908ed8ceac8ebaa3c28
李书伟 <lishuwei@tju.edu.cn>
2021-05-11 01:18:49 +0800

    配置多日志文件

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
-#include<log4cxx/logger.h>
-#include<log4cxx/propertyconfigurator.h>
+#include <log4cxx/logger.h>
+#include <log4cxx/basicconfigurator.h>
+#include <log4cxx/helpers/exception.h>
+#include <log4cxx/propertyconfigurator.h>
 
 using namespace log4cxx;
+using namespace log4cxx::helpers;
 
-int main()
 
-{
-	PropertyConfigurator::configure("../log4cxx.properties");
-	LoggerPtr rootLogger = Logger::getRootLogger();
-	LOG4CXX_INFO(rootLogger,"HHH");
 
-	return 0;
+int main(){
+    PropertyConfigurator::configure("../log4cxx.properties");
+    LoggerPtr logger_sendpro(Logger::getLogger("sendpro"));
+    LoggerPtr logger_msghander(Logger::getLogger("msghander"));
+	
+    int  i=0;
+    while(1)
+    {   
+	i++;
+	LOG4CXX_INFO(logger_sendpro," sendpro");
+	LOG4CXX_DEBUG(logger_sendpro,"This is a sendpro");
+	LOG4CXX_INFO(logger_msghander, "msghander");
+	LOG4CXX_DEBUG(logger_msghander,"This is a msghander");
+    }
+
+    return 0;
 }
